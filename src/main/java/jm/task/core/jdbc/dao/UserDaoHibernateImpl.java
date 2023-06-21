3a3cb7d378759a9e548e730635a732fcd0045ad2
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-06-22 02:04:21 +0300

    Before merge

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,60 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.HibernateUtil;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class UserDaoHibernateImpl implements UserDao {
+    private Transaction transaction = null;
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            //session.            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 
     @Override
-    public void dropUsersTable() {
-
-    }
+    public void dropUsersTable() {}
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            // start a transaction
+            transaction = session.beginTransaction();
+            // save the user object
+            User user = new User(name, lastName, age);
+            session.save(user);
+            // commit transaction
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void removeUserById(long id) {
-
-    }
+    public void removeUserById(long id) {}
 
     @Override
     public List<User> getAllUsers() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from User", User.class).list();           
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         return null;
     }
 
     @Override
-    public void cleanUsersTable() {
-
-    }
+    public void cleanUsersTable() {}
 }
---
+++ src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
@@ -0,0 +1,48 @@
+package jm.task.core.jdbc.util;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import jm.task.core.jdbc.model.User;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory = null;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                properties.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false");
+                properties.put(Environment.USER, "root");
+                properties.put(Environment.PASS, "root");
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+}
